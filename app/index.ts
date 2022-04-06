5f93ed7b791fa0739342b47d520af70349ad5b93
Antanas Šepikas <antanas.sepikas@telesoftas.com>
2022-04-06 21:30:03 +0300

    feat: add central error handling

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,7 @@ import { json } from 'body-parser';
 import { config } from 'dotenv';
 import * as express from 'express';
 import { controller as geolocationController } from './controllers/geo-location';
+import { errorHandler } from './lib/errors/express-error-handler';
 
 config();
 
@@ -12,6 +13,8 @@ export const app = express();
 app.use(json());
 
 geolocationController(app);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
